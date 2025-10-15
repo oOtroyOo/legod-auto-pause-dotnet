9470cdfc684d27745702b2f5a014bceb82dd627e
oOtroyOo <497790325@qq.com>
2025-10-15 17:46:18 +0800

    Wpf-Demo

---
+++ dotnet/LegodPause/App.xaml.cs
@@ -0,0 +1,48 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Threading;
+
+namespace LegodPause.Mvvm;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    [DllImport("Kernel32.dll")]
+    public static extern bool AttachConsole(int processId);
+
+
+    public static App CurrentApp => (App)Current;
+
+    /// <summary>
+    /// Occurs when the application is loading.
+    /// </summary>
+    private async void OnStartup(object sender, StartupEventArgs e)
+    {
+#if NETCOREAPP
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#endif
+        AttachConsole(-1);
+    }
+
+    /// <summary>
+    /// Occurs when the application is closing.
+    /// </summary>
+    private async void OnExit(object sender, ExitEventArgs e)
+    {
+    }
+
+    /// <summary>
+    /// Occurs when an exception is thrown by an application but not handled.
+    /// </summary>
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
+    }
+}
---
+++ dotnet/LegodPause/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,49 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using LegodPause.Console.Utilities;
+using Wpf.Ui.Appearance;
+
+namespace LegodPause.Console.Views.Pages;
+
+/// <summary>
+/// Interaction logic for SettingsPage.xaml
+/// </summary>
+public partial class SettingsPage
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+
+        AppVersionTextBlock.Text = $"WPF UI - Simple Demo - {GetAssemblyVersion()}";
+
+        if (Wpf.Ui.Appearance.ApplicationThemeManager.GetAppTheme() == ApplicationTheme.Dark)
+        {
+            DarkThemeRadioButton.IsChecked = true;
+        }
+        else
+        {
+            LightThemeRadioButton.IsChecked = true;
+        }
+
+        this.ApplyTheme();
+    }
+
+    private void OnLightThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        Wpf.Ui.Appearance.ApplicationThemeManager.Apply(ApplicationTheme.Light);
+    }
+
+    private void OnDarkThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        Wpf.Ui.Appearance.ApplicationThemeManager.Apply(ApplicationTheme.Dark);
+    }
+
+    private string GetAssemblyVersion()
+    {
+        return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+            ?? string.Empty;
+    }
+}
